1751f2bed35ccc280fe30df1f1098922a9bf7762
paco <paulo.constantino@nordicsemi.no>
2025-04-04 10:06:26 +0100

    wallace tree multiplier

--- systemverilog/experiments/comb_multiplier/comb_multiplier_tb.sv
+++ systemverilog/experiments/comb_multiplier/comb_multiplier_tb.sv
@@ -1,14 +1,15 @@
 module comb_multiplier_tb;
 
-  logic [23:0] a;
-  logic [23:0] b;
-  logic [47:0] result;
+  logic [7:0] a;
+  logic [7:0] b;
+  logic [15:0] result;
 
   initial begin
     a = '1; 
     b = '1; 
     #10us;
     $display("%d(%b) * %d(%b) = %d(%b)", $signed(a), a, $signed(b), b, $signed(result), result);
+    $display("result: %b", {2'(1'b1+1'b1)}[1]);
     $stop;
   end
 
