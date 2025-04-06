847c0a16233e0396881c2adfbe3e4c9ace221e67
paco <paulo.constantino@nordicsemi.no>
2025-04-06 02:20:06 +0100

    sv

--- systemverilog/experiments/comb_divider/comb_divider.sv
+++ systemverilog/experiments/comb_divider/comb_divider.sv
@@ -4,7 +4,7 @@ module comb_divider(
   output logic [3:0] quotient,
   output logic [3:0] remainder
 );
-  logic [4:0] partial;
+  logic signed [4:0] partial;
 
   // since operads are 4 bits each, the partials are formed by adding one extra 'sign' bit to it, so that we can perform
   // a signed subtraction for each run. this extra sign bit is then checked to decide whether the result was positive or negative.
@@ -45,5 +45,50 @@ module comb_divider(
     remainder = partial[3:0];
   end
 
+/*
+  logic [4:0] bb;
+  logic [4:0] partial;
+
+  always_comb begin
+    bb = {1'b0, ~b} + 5'b1;
+
+    partial = {4'b0, a[3]} + bb;
+    quotient[3] = partial[4];
+    if(quotient[3])
+      partial = {1'b0, partial[2:0], a[2]};
+    else begin
+      partial = partial - bb;
+      partial = {1'b0, partial[2:0], a[2]};
+    end
+
+    partial = partial + bb;
+
+    quotient[2] = partial[4];
+    if(quotient[2])
+      partial = {1'b0, partial[2:0], a[1]};
+    else begin
+      partial = partial - bb;
+      partial = {1'b0, partial[2:0], a[1]};
+    end
+
+    partial = partial + bb;
+
+    quotient[1] = partial[4];
+    if(quotient[1])
+      partial = {1'b0, partial[2:0], a[0]};
+    else begin
+      partial = partial - bb;
+      partial = {1'b0, partial[2:0], a[0]};
+    end
+
+    partial = partial + bb;
+
+    quotient[0] = partial[4];
+
+    if(!quotient[0]) begin
+      partial = partial - bb;
+    end
+    remainder   = partial[3:0];
+*/
 endmodule
 
